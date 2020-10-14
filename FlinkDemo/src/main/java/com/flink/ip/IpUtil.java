50b11f9ffce5767169cf9f3dc4399443d1ac0d99
lptnii@qq.com <lptnii@qq.com>
2020-10-14 14:25:40 +0800

    修正问题

--- FlinkDemo/src/main/java/com/flink/ip/IpUtil.java
+++ FlinkDemo/src/main/java/com/flink/ip/IpUtil.java
@@ -2,14 +2,10 @@ package com.flink.ip;
 import com.flink.entity.IpEntity;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import org.lionsoul.ip2region.DataBlock;
-import org.lionsoul.ip2region.DbConfig;
-import org.lionsoul.ip2region.DbMakerConfigException;
-import org.lionsoul.ip2region.DbSearcher;
-
 public class IpUtil {
-
    static IpUtil ipUtil = null;
+   static Okhttp okhttp = Okhttp.getInstance();
+   static final String ipHttp = "http://192.168.1.146:30011/ip?ip=";
    public static IpUtil getInstance() {
      if (ipUtil == null) {
        synchronized (IpUtil.class){
@@ -26,20 +22,12 @@ public class IpUtil {
      return ipUtil;
    }
 
-   org.lionsoul.ip2region.DbSearcher searcher = null;
-
   /**
    * 初始化ip
    * @return
-   * @throws DbMakerConfigException
    * @throws FileNotFoundException
    */
-   private IpUtil init() throws DbMakerConfigException, FileNotFoundException {
-     //根据ip进行位置信息搜索
-     org.lionsoul.ip2region.DbConfig config = new DbConfig();
-     //获取ip库的位置（放在src下）（直接通过测试类获取文件Ip2RegionTest为测试类）
-     String dbfile = IpUtil.class.getResource("/ip2region.db").getPath();
-     searcher = new DbSearcher(config, dbfile);
+   private IpUtil init() {
      return this;
    }
 
@@ -50,8 +38,7 @@ public class IpUtil {
    * @throws IOException
    */
    public IpEntity getIpInfo(String ip) throws IOException {
-     //采用Btree搜索
-     DataBlock block = searcher.btreeSearch(ip);
-     return new IpEntity(block.getRegion());
+     ip = okhttp.get(null, ipHttp+ip);
+     return new IpEntity(ip);
    }
 }
