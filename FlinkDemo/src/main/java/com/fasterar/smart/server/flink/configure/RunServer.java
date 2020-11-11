e54bb33fc5c67ebad8bf9489350dd867ab1b7797
lptnii@qq.com <lptnii@qq.com>
2020-11-11 09:54:18 +0800

    fink jar配置spring MyBatis-Plus

--- FlinkDemo/src/main/java/com/fasterar/smart/server/flink/configure/RunServer.java
+++ FlinkDemo/src/main/java/com/fasterar/smart/server/flink/configure/RunServer.java
@@ -3,8 +3,6 @@ package com.fasterar.smart.server.flink.configure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
-
 
 public class RunServer implements ApplicationListener<ContextRefreshedEvent> {
 
